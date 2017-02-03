dc3d86a9ce46b264466e8aaf4919486ac6c2fbd4
Chen Dong <raycdut@hotmail.com>
2017-02-03 20:50:47 +0800

    init

---
+++ RobotApolloSln/RobotApollo.WeChat.WebApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RobotApollo.WeChat.WebApp.MvcApplication" Language="C#" %>
---
+++ RobotApolloSln/RobotApollo.WeChat.WebApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RobotApollo.WeChat.WebApp.Startup))]
+namespace RobotApollo.WeChat.WebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
