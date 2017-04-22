587f03cc336bfe1ff1f2421adc9db0189812155e
raycdut <raycdut@hotmail.com>
2017-04-22 17:54:05 +0800

    add log 4net

--- RobotApolloSln/RobotApollo.SpiderMachine.App/Program.cs
+++ RobotApolloSln/RobotApollo.SpiderMachine.App/Program.cs
@@ -1,9 +1,14 @@
-﻿namespace RobotApollo.SpiderMachine.App
+﻿using System;
+namespace RobotApollo.SpiderMachine.App
 {
     class Program
     {
         static void Main(string[] args)
         {
+            LogHelper.WriteLog(typeof(Program), "Sprider machine start at:" + DateTime.Now.ToLongTimeString());
+
+            Console.ReadKey();
+
         }
     }
 }
